9d62d86d9e9ae672d25ec410edd7f46ce3983892
Audrey Agyemang <audreyagyemang310@gmail.com>
2019-07-08 21:26:05 -0400

    create apiRoutes.js file

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const axios = require("axios");
+const router = require("express").Router();
+
+// router.get("/recipeDate", (req, res) => {
+//   axios
+//     .get("", { params: req.query })
+//     .then(({ data: { results } }) => res.json(results))
+//     .catch(err => res.status(422).json(err));
+// });
+
+router.get("/nutritionData", (req, res) => {
+    axios
+      .get("https://us.openfoodfacts.org/", { params: req.query })
+      .then(({ data: { results } }) => res.json(results))
+      .catch(err => res.status(422).json(err));
+  });
+
+  router.get("/analyzeData", (req, res) => {
+    axios
+      .get("", { params: req.query })
+      .then(({ data: { results } }) => res.json(results))
+      .catch(err => res.status(422).json(err));
+  });
+  
+
+module.exports = router;
