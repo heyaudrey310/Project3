f5e85fa57a44971d3228059bc792f3f5038e37a5
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-07-10 19:21:23 -0400

    <p>

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
+// Dependencies
+// ***********************************
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-const Cors = require("cors");
+const cors = require("cors");
+
+const db = require("./models");
+
+
 // const routes = require("./routes");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
-// Define middleware here
+
+// Define Middleware here
+// ************************************
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(logger('dev'));
-app.use(Cors);
-//Serve up static assets (usually on heroku)
+app.use(express.static("public"));
+app.use(cors);
+
+//Serve up static assets (usually on HEROKU)
+// **********************************************
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Routes
+// *************************************
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // Connect to the Mongo DB
+// ****************************
  mongoose.connect(process.env.MONGODB_URI || "mongodb://whatthefruit:Aa03101990*@ds249267.mlab.com:49267/heroku_3jw23km5");
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+module.exports = app;
