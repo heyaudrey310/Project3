b46d5ac99207c04abcda7b052119f12e3d4ff6e3
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-07-10 21:13:21 -0400

    <linking pages>

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,29 +1,35 @@
 import React from 'react';
 // import logo from './logo.svg';
 import '../../../src/App.css';
-import '../Form/Form';
+import { Link } from 'react-router';
+// import '../Form/Form';
 
 function Home() {
   return (
     <div className="App">
     <div idname="root"></div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> What The Fruit</h1>
-        <button className="btn btn-primary" onClick={this.handleIncrement}>
-            Start Scanning Fruit
-          </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+     <header className="App-header">
+      <h1> What The Fruit</h1>
+        <button className="btn btn-primary"><Link to="/image">Start Scanning Fruit</Link></button>
+      </header>
     </div>
   );
 }
 
 export default Home;
+
+//  {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1> What The Fruit</h1>
+//         <button className="btn btn-primary" onClick={this.handleIncrement}>
+//             Start Scanning Fruit
+//           </button>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
---
+++ client/src/components/image/Image.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../image/style.css";
+
+function Image(){
+    return(
+        <div className="photo">
+            
+
+
+
+
+        </div>
+    );
+}
