4179149875ccb82af2b57a4af15c4b45b780c153
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-07-13 13:20:36 -0400

    <react pages>

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 // import logo from './logo.svg';
 import '../../../src/App.css';
-import { Link } from 'react-router';
-// import '../Form/Form';
+import {  BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Image from '../image/Image';
 
 function Home() {
   return (
-    <div className="App">
-    <div idname="root"></div>
-     <header className="App-header">
-      <h1> What The Fruit</h1>
-        <button className="btn btn-primary"><Link to="/image">Start Scanning Fruit</Link></button>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <div idname="root"></div>
+      <header className="App-header">
+        <h1> What The Fruit</h1>
+          <button className="btn btn-primary"><Link to="/image">Start Scanning Fruit</Link></button>
+        </header>
+        <Image></Image>
+      </div>
+    </Router>
   );
 }
 
