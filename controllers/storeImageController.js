4b828c70112945781735c5e61a34ec3fa998b9be
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-07-14 22:57:08 -0400

    <image CTRLr>

---
+++ controllers/storeImageController.js
@@ -0,0 +1,3 @@
+
+
+const storeImages = require('../models/savedImage')
---
+++ routes/storeImages.js
@@ -0,0 +1,30 @@
+
+module.exports = function(app) {
+    // const fs = require('fs')
+    const path = require('path')
+    const multer = require('multer')
+  
+    const storage = multer.diskStorage({
+      destination: function(req, file, cb) {
+        const uploadsDir = path.join(__dirname, '..', '..', 'public')
+        fs.mkdirSync(uploadsDir)
+        cb(null, uploadsDir)
+      },
+      filename: function(req, file, cb) {
+        cb(null, file.originalname)
+      } 
+    })
+  
+    const upload = multer({ storage })
+    const controller = require('../controllers/storeImageController')
+  
+    // define routes images uploads in relation to entry
+    app.route('/storeImages')
+       .get(controller.index)
+       .post(upload.single("data"), controller.create)
+  
+    app.route('/storeImages/:id')
+       .get(controller.show)
+       .put(controller.update)
+      //  .delete(controller.destroy)
+  }
