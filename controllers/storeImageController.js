0aac6527b8202fcf783719910cbd67c67d285bc0
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-07-15 00:36:31 -0400

    <controllers img>

--- controllers/storeImageController.js
+++ controllers/storeImageController.js
@@ -1,3 +1,15 @@
 
 
 const storeImages = require('../models/savedImage')
+
+exports.index = function(req, res) {
+    storeImages.find({storeImagesId: req.params.storeImages_id}, function (err, image) {
+        if (err) res.send(err)
+        res.json(image)
+    })
+      
+}
+
+exports.show = function(req,res) {
+
+}
