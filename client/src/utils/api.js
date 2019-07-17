63643d70edc9f7b77d229722c5e5cfe5576415cd
Nancy Pieh <stardust@Nancys-MacBook-Air.local>
2019-07-16 20:04:26 -0400

    <saved images>

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,10 +7,11 @@ export default {
         console.log("before axios")
         return axios.post("/api/storeImage", {
             imgBase64: image,
+            //{console.log(Image)}
         })
         .then(res => console.log("sent image"))
     },
-
+   
     getNutrition: function(food){
 
     },
